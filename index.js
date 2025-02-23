8881e4accc5fc968e9d99d3f93cad353f63e9a2e
Someswarklizos2001 <someswar@klizos.com>
2025-02-23 15:42:57 +0530

    push

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const http = require("http");
+const { ExpressPeerServer } = require("peer");
+const cors=require("cors");
+
+const app = express();
+const server = http.createServer(app);
+app.use(cors({
+    origin:"*"
+}))
+
+
+const peerServer = ExpressPeerServer(server, {
+    debug: true,
+    path: "/"
+});
+
+app.use("/peerjs", peerServer);
+
+app.get("/", (req, res) => {
+    res.send("PeerJS Server is running...");
+});
+
+server.listen(9000, () => {
+    console.log("PeerJS server running on port 9000");
+});
